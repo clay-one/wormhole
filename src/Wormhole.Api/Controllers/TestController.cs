0fae9bdcade271c0c0b8409dc2e6488af7204930
bahar taghavi <bahareh.t@fanap.plus>
2018-12-16 21:06:50 +0330

    add test controller

---
+++ src/Wormhole.Api/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using hydrogen.General.Validation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Wormhole.Api.Model;
+
+namespace Wormhole.Api.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public TestController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost("incoming")]
+        public async Task<IActionResult> InsertIncomingMessages([FromBody] PublishInput input)
+        {
+            if (_hostingEnvironment.EnvironmentName != "Development")
+            {
+                return NotFound();
+            }
+            return Ok(ApiValidationResult.Ok());
+        }
+    }
+}
