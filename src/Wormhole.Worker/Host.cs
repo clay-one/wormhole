30950e09d9bc9d80c5a805860d19c88127188de9
Kia Panahi Rad <kia.panahirad@gmail.com>
2018-12-28 17:17:59 +0330

    Add service registration (except kafka consumer services)

--- src/Wormhole.Worker/Host.cs
+++ src/Wormhole.Worker/Host.cs
@@ -1,16 +1,28 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using Confluent.Kafka;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Nebula;
+using Nebula.Queue;
 using NLog;
 using NLog.Config;
 using NLog.Extensions.Logging;
+using Wormhole.DataImplementation;
+using Wormhole.Interface;
+using Wormhole.Job;
+using Wormhole.Kafka;
+using Wormhole.Logic;
+using Wormhole.Utils;
 
 namespace Wormhole.Worker
 {
     internal class Host
     {
+        private static readonly NebulaContext NebulaContext = new NebulaContext();
+
         public static async Task Main(string[] args)
         {
             var host = new HostBuilder()
@@ -41,6 +53,24 @@ namespace Wormhole.Worker
 
                     builder.AddNLog(new NLogProviderOptions());
 
+                })
+                .ConfigureServices((hostContext, collection) =>
+                {
+                    collection.AddSingleton<ITenantDa, TenantDa>()
+                        .AddSingleton<IOutputChannelDa, OutputChannelDa>()
+                        .AddSingleton<IMessageLogDa, MessageLogDa>()
+                        .AddSingleton(NebulaContext)
+                        .AddScoped<IPublishMessageLogic, PublishMessageLogic>()
+                        .AddSingleton<IKafkaProducer, KafkaProducer>()
+                        .AddTransient<IKafkaConsumer<Null, string>, KafkaConsumer>()
+                        //.AddTransient<IConsumerBase, HttpPushOutgoingMessageConsumer>(sp =>
+                        //    new HttpPushOutgoingMessageConsumer(sp.GetService<IKafkaConsumer<Null, string>>(),
+                        //        sp.GetService<NebulaContext>(),
+                        //        sp.GetService<ILoggerFactory>(), ConsumerTopicName))
+                        .Configure<KafkaConfig>(hostContext.Configuration.GetSection(Constants.KafkaConfig))
+                        .Configure<RetryConfiguration>(
+                            hostContext.Configuration.GetSection(Constants.RetryConfiguration))
+                        .AddSingleton<IJobProcessor<HttpPushOutgoingQueueStep>, HttpPushOutgoingQueueProcessor>();
                 })
                 .UseConsoleLifetime()
                 .Build();
