6b3aaf52e3b285b19cb6d513bd03af31439b923e
bahar taghavi <bahareh.t@fanap.plus>
2019-01-29 16:23:34 +0330

    remove temp folder

---
+++ src/Wormhole.Api/Deployment/remove-temp-folder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+TMP_DIR=$1
+rm -rf $TMP_DIR
--- src/Wormhole.Api/Deployment/remove-temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash  
-DIR_NAME=$1 
-echo "Removing $DIR_NAME"
-if [ -d "$DIR_NAME" ]; then
-	rm -rf "$DIR_NAME"
-else
-	echo "It is not a directory"
-fi
