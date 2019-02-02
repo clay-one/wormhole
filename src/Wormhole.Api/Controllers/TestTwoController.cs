76958fc0cc3e0889bfce69410985a2894d18de4b
bahar taghavi <bahareh.t@fanap.plus>
2019-02-02 11:51:07 +0330

    test artifact drop

---
+++ src/Wormhole.Api/Controllers/TestTwoController.cs
@@ -0,0 +1,25 @@
+﻿using hydrogen.General.Validation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wormhole.Api.Controllers
+{
+    [Route("test-two")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class TestTwoController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public TestTwoController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost("incoming")]
+        public IActionResult InsertIncomingMessages([FromBody] object input)
+        {
+            return Ok("Hiiiiiii");
+        }
+    }
+}
