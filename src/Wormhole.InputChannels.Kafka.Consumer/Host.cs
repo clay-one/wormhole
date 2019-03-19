01c5382bab8e817ca744d87479628c042a287bbe
bahar taghavi <bahareh.t@fanap.plus>
2019-03-19 14:40:51 +0330

    log exceptions in kafka input channel

--- src/Wormhole.InputChannels.Kafka.Consumer/Host.cs
+++ src/Wormhole.InputChannels.Kafka.Consumer/Host.cs
@@ -1,11 +1,25 @@
-﻿namespace Wormhole.InputChannels.Kafka.Consumer
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Wormhole.InputChannels.Kafka.Consumer
 {
     internal class Host
     {
         public static void Main(string[] args)
         {
             var host = new WormholeHostBuilder().BuildHost(args);
-            host.StartAsync().GetAwaiter().GetResult();
+            var logger = host.Services.GetService<ILogger<Host>>();
+            try
+            {
+                host.StartAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Exception in kafka input channel application, ");
+                throw;
+            }
         }
     }
 }
